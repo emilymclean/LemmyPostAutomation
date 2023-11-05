420c081f19f77c1ec275c739d6ca4f65d6481428
Emily McLean <emily@mclean.lgbt>
2023-11-05 23:28:48 +1100

    Remove unnecessary lines

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ import os
 from plemmy import LemmyHttp
 
 from postautomation.automation import PostAutomation
-from postautomation.candidate import CSVCandidateProvider
-from postautomation.data.monitor_persistence import MonitorPersistence
-from postautomation.handlers.e621_handler import E621Handler
-from postautomation.handlers.furaffinity_handler import FuraffinityHandler
-from postautomation.monitor import PostMonitor
-from postautomation.scraper import Scraper
 
 if __name__ == "__main__":
     lemmy = LemmyHttp(os.environ["instance"])
@@ -20,5 +14,3 @@ if __name__ == "__main__":
         os.environ["schedule"]
     )
     automation.run()
-
-    pass
